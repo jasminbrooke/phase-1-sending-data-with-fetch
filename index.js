aea7aed686324cba534a6061648523185f6bd309
+ <+>
2022-03-10 18:23:27 -0500

    commit tree cbfeede65666544648faa09e143608cd734ca5f6

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+const submitData = (name, email) => {
+    const formData = {
+        name,
+        email,
+      };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(formData),
+      };
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then((response) => response.json())
+        .then((object) => document.querySelector("body").innerHTML = object.id)
+        .catch(message => document.querySelector("body").innerHTML = message)
+}
